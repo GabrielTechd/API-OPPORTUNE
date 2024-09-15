f55ce50e1b57caa96889da997b889929c46a4967
GabrielTech <bielneres7@gmail.com>
2024-09-15 18:26:29 -0300

    mundaças no codigo de login

--- routes/publicas/login.js
+++ routes/publicas/login.js
@@ -1,9 +1,9 @@
-// routes/login.js
 import express from "express";
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import { body, validationResult } from "express-validator";
 
 // Carregar variáveis de ambiente
 dotenv.config();
@@ -13,15 +13,23 @@ const router = express.Router();
 
 // Use a chave secreta da variável de ambiente
 const JWT_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET não está definido no ambiente");
+}
 
-// Rota de Login
-router.post("/login", async (req, res) => {
-  const { email, senha } = req.body;
+// Validação e sanitização de entrada
+const validateLogin = [
+  body('email').isEmail().withMessage('E-mail inválido'),
+  body('senha').isLength({ min: 6 }).withMessage('Senha deve ter pelo menos 6 caracteres')
+];
 
-  // Verifique se o e-mail e a senha foram fornecidos
-  if (!email || !senha) {
-    return res.status(400).json({ message: "E-mail e senha são obrigatórios" });
+router.post("/login", validateLogin, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
+
+  const { email, senha } = req.body;
 
   try {
     // Encontre o usuário em ambas as tabelas
@@ -33,6 +41,7 @@ router.post("/login", async (req, res) => {
       userType = 'empresa'; // Se encontrado na tabela 'empresa', define o tipo como 'empresa'
     }
 
+    // Se o usuário não for encontrado ou a senha não corresponder
     if (!user || !(await bcrypt.compare(senha, user.senha))) {
       return res.status(400).json({ message: "E-mail ou senha inválidos" });
     }
@@ -41,7 +50,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user.id, tipo: userType }, JWT_SECRET, { expiresIn: "10d" });
 
     // Responda com o token JWT e dados do usuário
-    res.status(200).json({ userType });
+    res.status(200).json({ token, userType });
   } catch (err) {
     console.error("Erro ao fazer login:", err);
     res.status(500).json({ message: "Erro no servidor, tente novamente" });
